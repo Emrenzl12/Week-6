53e7bc1ce9dc0b5070ea9a59aed96e8a62da7fbc
Emrenzl12 <nazli.emre@ibu.edu.mk>
2026-05-10 16:25:17 +0200

    Create solution.py

---
+++ week6/projects/project1/solution.py
@@ -0,0 +1,8 @@
+# Project 1 - Temperature Converter
+# Author: Emre Nazli
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"{celsius:.1f}°C = {fahrenheit:.1f}°F")
