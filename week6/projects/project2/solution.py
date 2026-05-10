7970160056baf7156dbbce60e2dbb588a30335e4
Emrenzl12 <nazli.emre@ibu.edu.mk>
2026-05-10 16:31:16 +0200

    Create solution.py

---
+++ week6/projects/project2/solution.py
@@ -0,0 +1,24 @@
+# Project 2 - Number Guessing Game
+# Author: Emre Nazli
+
+import random
+
+secret_number = random.randint(1, 10)
+
+guesses = 0
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+while guess != secret_number:
+    guesses += 1
+
+    if guess < secret_number:
+        print("Too low!")
+    else:
+        print("Too high!")
+
+    guess = int(input("Try again: "))
+
+guesses += 1
+
+print(f"Congratulations! You guessed it in {guesses} guesses.")
